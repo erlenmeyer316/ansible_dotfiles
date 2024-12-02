0fa2cc6222e6357d310a5f6a91fa0444d92598d1
Adam M. Dickison <adam.dickison@posteo.net>
2024-12-02 15:12:08 -0500

    remote output from ssh init

--- roles/bash/files/bash/ssh_init.sh
+++ roles/bash/files/bash/ssh_init.sh
@@ -14,8 +14,8 @@ if ! kill -0 $SSH_AGENT_PID &> /dev/null; then
     echo "$SSH_AGENT_PID" > "$ssh_pid_file"
     ssh-add -A 2> /dev/null
     >&2 echo "Started ssh-agent with '$SSH_AUTH_SOCK'"
-else
-    >&2 echo "ssh-agent on '$SSH_AUTH_SOCK' ($SSH_AGENT_PID)"
+#else
+#    >&2 echo "ssh-agent on '$SSH_AUTH_SOCK' ($SSH_AGENT_PID)"
 fi
 export SSH_AGENT_PID
 export SSH_AUTH_SOCK
