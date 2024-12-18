a17077d81dfd7e76c0728aeefda2b8f72a661fb7
Adam M. Dickison <adam.dickison@posteo.net>
2024-12-17 19:18:07 -0500

    update

---
+++ roles/bash/files/bash/kpxc_functions.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+kpxc_get_secret() {
+	# check if argument was passed make
+	# check if keepassxc-cli is installed
+	# check if .kpxc/conf exists
+	# check if .ssh/cert exists
+}
